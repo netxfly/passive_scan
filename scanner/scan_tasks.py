02a35412d78af250435d88eddef458bded8f16c1
c4bbage <c4bbage@qq.com>
2015-08-08 23:32:49 +0800

    scanning http POST method request
    scanning http POST method request

--- scanner/scan_tasks.py
+++ scanner/scan_tasks.py
@@ -14,7 +14,7 @@ app = Celery('task', backend=BACKEND_URL, broker=BROKER_URL)
 def scan(task_id, task_url,domain,method,request_data,user_agent,cookies):
     if task_url:
         print "start to scan %s, task_id: %s" % (task_url, task_id)
-        scanner = arachni_console.Arachni_Console(task_url, user_agent, cookies,page_limit=1)
+        scanner = arachni_console.Arachni_Console(task_url, method,user_agent, cookies,request_data,page_limit=1)
         report = scanner.get_report()
         if report:
             reporter = Reporter(report)
